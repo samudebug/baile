1f238dd488d1c3237518d0cb4b042987dbe9232a
Samuel Martins <samuel.ubuntu@gmail.com>
2021-02-18 22:32:46 -0300

    Broken production fix

--- src/youtube-video/YoutubeVideo.js
+++ src/youtube-video/YoutubeVideo.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './YoutubeVideo.css';
 import moldura from '../assets/moldura.png'
 import { Image } from 'react-bootstrap';
+import ReactPlayer from 'react-player/lazy';
 function YoutubeVideo() {
     
-    const youtubeVideo = "https://www.youtube.com/embed/videoseries?list=PLU8MThNhi5nM_topLHAJYqtufy-IuYpJJ";
+    const youtubeVideo = "https://www.youtube.com/watch?v=VgN7shMqsIE&list=PLU8MThNhi5nM_topLHAJYqtufy-IuYpJJ";
     return (
         <div className="video-wrapper">
             <Image src={moldura} className="moldura"/>
-            <iframe  title="video-highlight" className="video" src={youtubeVideo} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div className="video">
+            <ReactPlayer 
+            url={youtubeVideo}
+            height="100%"
+            width="100%"
+            />
+            </div>
         </div>
     )
 }
